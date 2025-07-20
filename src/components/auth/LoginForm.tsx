f2764643b589bb12f67bdc6d749ed9643bdf4e20
Kavya gp <kavyagp2108@gmail.com>
2025-07-20 16:25:09 +0000

    add the login not sign in

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -206,7 +206,7 @@ export default function LoginForm() {
             />
             <Button type="submit" className="w-full" disabled={isLoading}>
             {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-            Sign In
+            Login
             </Button>
             <Button variant="outline" type="button" className="w-full" onClick={handleGoogleSignIn} disabled={isLoading}>
             {isLoading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : <svg className="mr-2 h-4 w-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 126 21.2 177.2 56.4l-64.4 64.4c-33.3-30.3-76.2-49.2-124.7-49.2-94.3 0-171.3 76.4-171.3 171.3s77 171.3 171.3 171.3c108.3 0 151.8-83.3 155.8-123.3H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path></svg>}
